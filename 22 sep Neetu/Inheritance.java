0d0d9066821da9d0049b9aeab7f3b5641ad44e93
Tutuu123 <113002739+Tutuu123@users.noreply.github.com>
2022-09-22 18:47:03 +0530

    Add files via upload

---
+++ 22 sep Neetu/Inheritance.java
@@ -0,0 +1,63 @@
+ package lab3;
+
+
+public class GrandMother
+ {
+	
+
+void relation1()
+
+{
+
+System.out.println("one daughter");
+
+}
+
+void relation2()
+	
+{
+		
+System.out.println(" 2 granddaughter");
+	
+}
+	
+	
+
+}
+ public class Mother extends GrandMother
+ {
+
+	
+void relation1()
+	
+{
+	    
+super.relation1();
+		
+System.out.println(" 2 daughter");
+
+}
+
+} 
+public class Daughter extends Mother
+{
+	
+	
+void relation2()
+	
+{   
+		
+System.out.println("  brother");
+
+}
+
+class Inheritance
+public static void main(String args[])
+
+{
+Daughter d=new Daughter();
+d.relation1();
+d.relation2();
+
+}
+}
---
+++ 22 sep Neetu/Ques1.java
@@ -0,0 +1,57 @@
+class bank
+{ 
+ 
+int getRateOfInterest()
+
+{
+
+return 4;
+
+}  
+
+}  
+ 
+
+class BOI extends bank
+
+{  
+int getRateOfInterest()
+
+{
+
+return 16;
+
+}  
+
+} 
+ 
+class ICICI extends bank
+
+{  
+
+int getRateOfInterest()
+
+{
+
+return 2;
+
+} 
+ 
+}
+ 
+
+class Ques1{  
+
+public static void main(String args[])
+
+{  
+
+BOI s=new BOI();
+  ICICI a=new ICICI(); 
+ 
+System.out.println("SBI Rate of Interest: "+s.getRateOfInterest()); 
+ 
+System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());  
+}
+  
+}
